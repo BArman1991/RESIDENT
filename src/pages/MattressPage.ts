a9de97ea41cc6ea89cfd2b150f9ba2975608fe62
BArman1991 <armanbelikov@gmail.com>
2025-10-25 23:19:50 +0300

    fix flow for add a product, closing overlay bunner

--- src/pages/MattressPage.ts
+++ src/pages/MattressPage.ts
@@ -11,13 +11,18 @@ export class MattressPage {
     return this.page.locator(':is([data-testid*="_item_the-awara-"])').first();
   }
 
+  private get closeOverlayBunner() {
+    return this.page.locator(':is([class="dy-lb-close"])')
+  }
 
   async addFirstProductToCart() {
+    // Wait for the page to load
     await this.page.waitForLoadState('domcontentloaded');
     await this.page.waitForLoadState('networkidle', { timeout: 20000 });
+    // Close the overlay bunner
+    await this.closeOverlayBunner.click();
+    // Add the product to the cart
     const btn = this.addToCartButton.first();
-    await btn.click();
-    await this.upgradeElement.waitFor({ state: "visible", timeout: 10000 });
     await btn.click();
   }
 }
